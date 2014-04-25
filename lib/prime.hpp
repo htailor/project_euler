da25f6c2a96b9d2919dec7d7f1685ef2e639cfac
Hemant Tailor <hemant_tailor_2010@hotmail.com>
2014-04-25 19:31:18 +0100

    Added problem7 - incomplete.

---
+++ lib/prime.hpp
@@ -0,0 +1,11 @@
+#ifndef _PRIME_
+#define _PRIME_
+
+bool isPrime(unsigned int &x){
+
+
+
+	return false;
+}
+
+#endif
---
+++ problem7.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************************
+
+	Problem 7 - 10001st prime
+
+	By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the
+	6th prime is 13.
+
+	What is the 10001st prime number?
+
+*******************************************************************************************/
+
+#include <iostream>
+#include <set>
+#include <runtime.hpp>
+
+
+
+int Answer1(const int &nth_prime){
+
+	return prime_number;
+}
+
+
+int main(int argc, char* argv[]){
+
+	std::cout << "10001st prime" << std::endl;
+	std::cout << "=============" << std::endl;
+
+	Runtime timer;
+	timer.Start();
+	std::cout << "Answer: " << Answer1(10001) << std::endl;
+	timer.Stop();
+
+	return 0;
+}
